ec0e777ba683d91a2dc3b6783b2b74ee37a3719b
biantsh <shkolollibiant@gmail.com>
2024-03-27 16:17:21 +0100

    Simplify init of 0-filled array

--- part-1/anecdotes/src/App.jsx
+++ part-1/anecdotes/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0);
-  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0, 0]);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
 
   const handleVote = () => {
     const votesCopy = [...votes];
