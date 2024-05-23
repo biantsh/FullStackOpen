c181b2bfb7cbda5d01ff5696be89baa7417bd0d4
biantsh <shkolollibiant@gmail.com>
2024-05-23 11:18:02 +0200

    Add exercise 2.11

--- part-2/phonebook/src/App.jsx
+++ part-2/phonebook/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [nameFilter, setNameFilter] = useState('');
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  }
+  , []);
 
   const handleFilterChange = (event) => {
     setNameFilter(event.target.value);
