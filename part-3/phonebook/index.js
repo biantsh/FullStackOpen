547340d793ffae4b8e363f15612a765f3154c553
Biant Shkololli <shkolollibiant@gmail.com>
2024-05-26 20:29:58 +0200

    Add exercise 3.3

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -24,13 +24,24 @@ let persons = [
     }
 ]
 
+app.get('/info', (request, response) => {
+    const date = new Date().toString();
+    response.send(`<p>Phonebook has info for ${persons.length} people.</p> <p>${date}</p>`);
+});
+
 app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
 
-app.get('/info', (request, response) => {
-    const date = new Date().toString();
-    response.send(`<p>Phonebook has info for ${persons.length} people.</p> <p>${date}</p>`);
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(p => p.id === id);
+
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
 });
 
 const PORT = 3001;
