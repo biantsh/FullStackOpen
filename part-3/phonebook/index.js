737f598bc4c7d4ce26d5cdce068dec3bc7d0aaa3
Biant Shkololli <shkolollibiant@gmail.com>
2024-05-26 20:57:35 +0200

    Add exercise 3.6

--- part-3/phonebook/index.js
+++ part-3/phonebook/index.js
@@ -53,6 +53,18 @@ app.get('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body;
 
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'Name or number is missing.'
+        });
+    }
+
+    if (persons.find(p => p.name === body.name)) {
+        return response.status(400).json({
+            error: 'Name must be unique'
+        });
+    }
+
     const person = {
         "id": generateId(),
         "name": body.name,
